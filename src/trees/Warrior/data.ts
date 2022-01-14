c86dc1ed0c59bb4e31bfcf21a1cac4d5b51e2e18
Jay Walker <jaywalkercooks@gmail.com>
2022-01-14 17:29:02 +0000

    Update src/trees/Warrior/data.ts

--- src/trees/Warrior/data.ts
+++ src/trees/Warrior/data.ts
@@ -1,4 +1,4 @@
-import { areReqPointsMet, TalentData } from "../../TalentContext";
+import { TalentData } from "../../TalentContext";
 import { talentText, requireAll } from "../../utils";
 
 const backgrounds = requireAll(
